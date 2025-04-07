d1ef86cb4b96fbb3601aa950476d888288afff9f
Ben Wing <ben@benwing.com>
2025-04-07 04:24:05 -0500

    find_red_links_on_page: expand and generalize

--- find_red_links_on_page.py
+++ find_red_links_on_page.py
@@ -11,17 +11,11 @@ import blib
 from blib import getparam, rmparam, msg, site, tname
 
 
-punc_chars = "".join("\\" + unichr(i) for i in range(sys.maxunicode)
-    if unicodedata.category(unichr(i)).startswith('P'))
+#punc_chars = "".join("\\" + unichr(i) for i in range(sys.maxunicode)
+#    if unicodedata.category(unichr(i)).startswith('P'))
 
-tbl = dict.fromkeys(i for i in range(sys.maxunicode)
-                          if unicodedata.category(unichr(i)).startswith('P'))
-def remove_punctuation(text):
-      return text.translate(tbl)
-
-blib.getData()
-
-def remove_diacritics(text, langcode):
+# WARNING: May not work any more.
+def fast_remove_diacritics(text, langcode):
   if langcode not in blib.languages_byCode:
     return text
   if "entryNamePatterns" in blib.languages_byCode[langcode]:
@@ -38,22 +32,17 @@ def remove_diacritics(text, langcode):
     )
   return text
 
-def process_page(page, index, templates):
-  pagetitle = str(page.title())
+def process_text_on_page(index, pagetitle, text):
   def pagemsg(txt):
     msg("Page %s %s: %s" % (index, pagetitle, txt))
-  parsed = blib.parse_text(str(page.text))
-  for t in parsed.filter_templates():
-    if tname(t) in templates:
-      lang = getparam(t, "1")
-      if lang not in blib.languages_byCode:
-        pagemsg("WARNING: Unrecognized language code %s" % lang)
-        continue
-      langname = blib.languages_byCode[lang]["canonicalName"]
-      term = getparam(t, "2")
-      pagenm = remove_diacritics(term, lang)
-      if not pagenm:
-        continue
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, args.verbose)
+  def remove_diacritics(text, langcode):
+    return expand_text("{{#invoke:languages/templates|getByCode|%s|makeEntryName|%s}}" % (langcode, text))
+  def check_one_link(langname, pagenm, term=None):
+    if not pagenm:
+      outtext = "null link specified"
+    else:
       if pagenm.startswith("*"):
         pagenm = "Reconstruction:%s/%s" % (langname, pagenm[1:])
       page = pywikibot.Page(site, pagenm)
@@ -61,24 +50,61 @@ def process_page(page, index, templates):
         text = str(page.text)
         if re.search("#redirect", text, re.I):
           outtext = "exists as redirect"
-        elif "==%s==" % langname in text:
-          outtext = "exists"
         else:
-          outtext = "exists only in some other language"
+          sections, sections_by_lang, section_langs = blib.split_text_into_sections(text, pagemsg)
+          if langname in sections_by_lang:
+            outtext = "exists in %s" % langname
+          else:
+            existing_langs = [lang for secno, lang in section_langs]
+            outtext = "exists in other languages %s" % ", ".join(existing_langs)
       else:
         outtext = "does not exist"
-      end
-      if term == pagenm:
-        pagemsg("%s [[%s]] %s" % (langname, pagenm, outtext))
-      else:
-        pagemsg("%s [[%s|%s]] %s" % (langname, pagenm, term, outtext))
+    term = term or pagenm
+    if term == pagenm:
+      pagemsg("%s [[%s]] %s" % (langname, pagenm, outtext))
+    else:
+      pagemsg("%s [[%s|%s]] %s" % (langname, pagenm, term, outtext))
+  def check_text(text):
+    if args.check_raw_links:
+      for m in re.finditer(r"\[\[(.*?)\]\]", text):
+        pagenm = m.group(1)
+        check_one_link(args.langname, pagenm)
+    if templates:
+      parsed = blib.parse_text(text)
+      for t in parsed.filter_templates():
+        if tname(t) in templates:
+          lang = getparam(t, "1")
+          if lang not in blib.languages_byCode:
+            pagemsg("WARNING: Unrecognized language code %s" % lang)
+            continue
+          langname = blib.languages_byCode[lang]["canonicalName"]
+          term = getparam(t, "2")
+          pagenm = remove_diacritics(term, lang)
+          if not pagenm:
+            continue
+          check_one_link(langname, pagenm, term)
+  if args.check_only_defns:
+    lines = text.split("\n")
+    for line in lines:
+      if re.search("^#+[^*:]", line):
+        check_text(line)
+  else:
+    check_text(text)
 
-parser = blib.create_argparser("Find red/yellow links", include_pagefile=True)
-parser.add_argument("--templates", help="Comma-separated list of templates to check", required=True)
+parser = blib.create_argparser("Find red/yellow links", include_pagefile=True, include_stdin=True)
+parser.add_argument("--templates", help="Comma-separated list of templates to check")
+parser.add_argument("--check-raw-links", help="If true, check raw links", action="store_true")
+parser.add_argument("--check-only-defns", help="If true, check only defn lines", action="store_true")
+parser.add_argument("--langname", help="Language name for raw links")
 args = parser.parse_args()
 start, end = blib.parse_start_end(args.start, args.end)
 
-templates = args.templates.split(",")
-def do_process_page(page, index):
-  process_page(page, index, templates)
-blib.do_pagefile_cats_refs(args, start, end, do_process_page)
+if args.check_raw_links and not args.langname:
+  raise ValueError("--langname must be specified if --check-raw-links specified")
+if args.templates:
+  templates = args.templates.split(",")
+  blib.getData()
+else:
+  templates = []
+
+blib.do_pagefile_cats_refs(args, start, end, process_text_on_page, stdin=True)
