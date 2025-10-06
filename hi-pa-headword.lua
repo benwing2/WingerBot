69537c5da4e4b65b7f00890c586bffa2c07fbce0
Ben Wing <ben@benwing.com>
2025-10-06 03:04:14 -0500

    hi-pa-headword: partial work on supporting inline modifiers with comma-separated items (NOT DONE)

--- hi-pa-headword.lua
+++ hi-pa-headword.lua
@@ -9,58 +9,82 @@ local rfind = mw.ustring.find
 local rmatch = mw.ustring.match
 local rsplit = mw.text.split
 
+local list_param = {list = true, disallow_holes = true}
+local boolean_param = {type = "boolean"}
+local gender_param = {type = "genders"}
+local gender_param_with_default = {type = "genders", default = "?"}
+
 local function glossary_link(anchor, text)
 	text = text or anchor
 	return "[[Appendix:Glossary#" .. anchor .. "|" .. text .. "]]"
 end
 
-local function process_genders(data, genders)
-	for _, g in ipairs(genders) do
+local function validate_genders(data, genders)
+	data.genders = genders
+	for _, gspec in ipairs(genders) do
+		local g = gspec.g
 		if g == "m" or g == "f" or g == "m-p" or g == "f-p" or g == "mf" or g == "mf-p" or g == "mfbysense" or g == "mfbysense-p" or g == "?" then
-			table.insert(data.genders, g)
 		else
-			error("Invalid gender: " .. (g or "(nil)"))
+			error("Invalid gender: " .. g)
 		end
 	end
 end
 
+-- Parse an inflection. The raw arguments come from `args[field]`, which is parsed for inline modifiers. Multiple
+-- comma-separated values are allowed.
+local function parse_inflection(data, args, field, is_head)
+	local argfield = field
+	if type(argfield) == "table" then
+		argfield = argfield[1]
+	end
+	return m_headword_utilities.parse_term_list_with_modifiers {
+		forms = args[argfield],
+		paramname = field,
+		splitchar = ",",
+		is_head = is_head,
+		include_mods = not is_head and {"tr"} or nil,
+	}
+end
+
+-- Parse and insert an inflection not requiring additional processing into `data.inflections`. The raw arguments come
+-- from `args[field]`, which is parsed for inline modifiers. Multiple comma-separated values are allowed. `label` is the
+-- label that the inflections are given; sections enclosed in <<...>> are linked to the glossary. `accel_form` is the
+-- accelerator form, or nil.
+local function parse_and_insert_inflection(data, args, field, label, accel_form)
+	local terms = parse_inflection(data, args, field)
+	m_headword_utilities.insert_inflection {
+		headdata = data,
+		terms = terms,
+		label = label,
+		accel = accel_form and {form = accel_form} or nil,
+	}
+end
+
 export.pos_functions.adjectives = {
 	params = {
-		["comparative"] = {},
-		["superlative"] = {},
-		[1] = {alias_of = "comparative"},
-		[2] = {alias_of = "superlative"},
-		["f"] = {list = true},
-		["m"] = {list = true},
-		["ind"] = {type = "boolean"},
+		[1] = {list = "comp", disallow_holes = true},
+		[2] = {list = "sup", disallow_holes = true},
+		["f"] = list_param,
+		["m"] = list_param,
+		["ind"] = boolean_param,
 	},
 	func = function(args, data)
 		if args["ind"] then
 			table.insert(data.inflections, {label = glossary_link("indeclinable")})
 			table.insert(data.categories, data.langname .. " indeclinable adjectives")
 		end
-		if args["comparative"] then
-			table.insert(data.inflections, {label = "comparative", args["comparative"]})
-		end
-		if args["superlative"] then
-			table.insert(data.inflections, {label = "superlative", args["superlative"]})
-		end
-		if #args["m"] > 0 then
-			args["m"].label = "masculine"
-			table.insert(data.inflections, args["m"])
-		end
-		if #args["f"] > 0 then
-			args["f"].label = "feminine"
-			table.insert(data.inflections, args["f"])
-		end
+		parse_and_insert_inflection(data, args, {1, "comp"}, "<<comparative>>")
+		parse_and_insert_inflection(data, args, {2, "sup"}, "<<superlative>>")
+		parse_and_insert_inflection(data, args, "m", "masculine")
+		parse_and_insert_inflection(data, args, "f", "feminine")
 	end,
 }
 
 export.pos_functions.ordinals = {
 	params = {
-		["f"] = {list = true},
-		["m"] = {list = true},
-		["ind"] = {type = "boolean"},
+		["f"] = list_param,
+		["m"] = list_param,
+		["ind"] = boolean_param,
 	},
 	func = function(args, data)
 		data.pos_category = "adjectives"
@@ -69,62 +93,50 @@ export.pos_functions.ordinals = {
 			table.insert(data.inflections, {label = glossary_link("indeclinable")})
 			table.insert(data.categories, data.langname .. " indeclinable numerals")
 		end
-		if #args["m"] > 0 then
-			args["m"].label = "masculine"
-			table.insert(data.inflections, args["m"])
-		end
-		if #args["f"] > 0 then
-			args["f"].label = "feminine"
-			table.insert(data.inflections, args["f"])
-		end
+		parse_and_insert_inflection(data, args, "m", "masculine")
+		parse_and_insert_inflection(data, args, "f", "feminine")
 	end,
 }
 
 export.pos_functions.cardinals = {
 	params = {
-		["g"] = {list = true},
-		["sym"] = {list = true},
+		[1] = gender_param,
+		["g"] = {alias_of = 1}, -- FIXME, delete
+		["g2"] = true, -- FIXME, delete
+		["sym"] = list_param,
 	},
 	func = function(args, data)
 		data.pos_category = "numerals"
-		process_genders(data, args["g"])
-		if #args["sym"] > 0 then
-			args["sym"].label = "native script symbol"
-			table.insert(data.inflections, args["sym"])
-		end
+		validate_genders(data, args[1])
+		parse_and_insert_inflection(data, args, "sym", "native script symbol")
 	end,
 }
 
 local function nouns(plpos)
 	return {
 		params = {
-			["g"] = {list = true, default = "?"},
-			["pl"] = {list = true},
-			["f"] = {list = true},
-			["m"] = {list = true},
-			["ind"] = {type = "boolean"},
+			[1] = gender_param_with_default,
+			["g"] = {alias_of = 1}, -- FIXME, delete
+			["g2"] = true, -- FIXME, delete
+			["pl"] = list_param,
+			["f"] = list_param,
+			["m"] = list_param,
+			["ind"] = boolean_param,
 		},
 		func = function(args, data)
-			process_genders(data, args["g"])
+			validate_genders(data, args[1])
 			if args["ind"] then
-				if #args["pl"] > 0 then
+				if args["pl"][1] then
 					error("Can't specify both ind= and pl=")
 				end
 				table.insert(data.inflections, {label = glossary_link("indeclinable")})
 				table.insert(data.categories, data.langname .. " indeclinable " .. plpos)
-			elseif #args["pl"] > 0 then
-				args["pl"].label = "plural"
-				table.insert(data.inflections, args["pl"])
+			else
+				parse_and_insert_inflection(data, args, "pl", "plural")
 			end
-			if #args["m"] > 0 then
-				args["m"].label = "masculine"
-				table.insert(data.inflections, args["m"])
-			end
-			if #args["f"] > 0 then
-				args["f"].label = "feminine"
-				table.insert(data.inflections, args["f"])
-			end
-			if #args["m"] > 0 or #args["f"] > 0 then
+			parse_and_insert_inflection(data, args, "m", "masculine")
+			parse_and_insert_inflection(data, args, "f", "feminine")
+			if args["m"][1] or args["f"][1] then
 				table.insert(data.categories, data.langname .. " " .. plpos .. " with other-gender equivalents")
 			end
 		end,
@@ -136,21 +148,20 @@ export.pos_functions["proper nouns"] = nouns("proper nouns")
 
 export.pos_functions.pronouns = {
 	params = {
-		["g"] = {list = true},
+		[1] = gender_param,
+		["g"] = {alias_of = 1}, -- FIXME, delete
+		["g2"] = true, -- FIXME, delete
 	},
 	func = function(args, data)
-		process_genders(data, args["g"])
+		validate_genders(data, args[1])
 	end,
 }
 
 export.pos_functions.verbs = {
 	params = {
-		[1] = {},
-		["g"] = {list = true},
+		[1] = true,
 	},
 	func = function(args, data)
-		data.genders = args["g"]
-
 		if args[1] then
 			local label, cat
 			if args[1] == "t" then
@@ -183,10 +194,12 @@ export.pos_functions.verbs = {
 local function pos_with_gender()
 	return {
 		params = {
-			["g"] = {list = true},
+			[1] = gender_param,
+			["g"] = {alias_of = 1}, -- FIXME, delete
+			["g2"] = true, -- FIXME, delete
 		},
 		func = function(args, data)
-			data.genders = args["g"]
+			validate_genders(data, args[1])
 		end,
 	}
 end
