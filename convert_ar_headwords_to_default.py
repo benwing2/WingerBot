fadcf61238166668c07f136481f910e95f127ae1
Ben Wing <ben@benwing.com>
2025-10-26 03:13:56 -0500

    convert_ar_headwords_to_default: support +f for sound feminine plural of masculines, fix -āh plurals

--- convert_ar_headwords_to_default.py
+++ convert_ar_headwords_to_default.py
@@ -10,6 +10,9 @@ from blib import getparam, rmparam, tname, pname, msg, site
 class BadTranslitException(Exception):
   pass
 
+class ContinueException(Exception):
+  pass
+
 def check_tr_ending(tr, expected, replacement):
   if not tr:
     return tr
@@ -49,7 +52,17 @@ def default_masculine_plural(term, tr):
   return term, tr
 
 def default_feminine_plural(term, tr):
-  if term.endswith(ar.AN + ar.AMAQ) or term.endswith(ar.AN + ar.ALIF):
+  # صَلَاة pl. صَلَوَات and أَدَاة pl. أَدَوَات and similar; but نَوَاة and وَفَاة with a و in them become نَوَيَات and وَفَيَات;
+  # and longer terms like مُبَارَاة and كُمَّثْرَاة invariably form their plural in -يَات.
+  m = re.search("^([^و]" + ar.A + "[^و])" + ar.AAH + "$", term)
+  if m:
+    term = m.group(1) + ar.A + ar.W + ar.AAT
+    tr = check_tr_ending(tr, "āh", "awāt")
+    return term, tr
+  if term.endswith(ar.AAH):
+    term = term[:-3] + ar.AYAAT
+    tr = check_tr_ending(tr, "āh", "ayāt")
+  elif term.endswith(ar.AN + ar.AMAQ) or term.endswith(ar.AN + ar.ALIF):
     term = term[:-2] + ar.AYAAT
     tr = check_tr_ending(tr, "an", "ayāt")
   elif term.endswith(ar.HAMZA + ar.IN):
@@ -135,7 +148,7 @@ def canonicalize_form(term, tr):
   elif term.endswith(ar.AAT + ar.UN):
     term = term[:-1]
     tr = check_tr_ending(tr, "un", "")
-  elif term.endswith(ar.TAM) and not term.endswith(ar.AH) and not term.endswith(ar.ALIF + ar.TAM):
+  elif term.endswith(ar.TAM) and not term.endswith(ar.AH) and not term.endswith(ar.ALIF + ar.TAM) and not term.endswith(ar.AMAD + ar.TAM):
     term = term[:-1] + ar.AH
   elif term.endswith(ar.ALIF + ar.TAM) and not term.endswith(ar.AAH):
     term = term[:-2] + ar.AAH
@@ -181,7 +194,8 @@ def process_text_on_page(index, pagetitle, text):
   }
   noun_inflections = [
     ("d", "dual", lambda g: default_feminine_dual if g == "f" else default_masculine_dual),
-    ("pl", "plural", lambda g: default_feminine_plural if g == "f" else default_masculine_plural),
+    ("pl", "plural", lambda g: default_feminine_plural if g == "f" else [
+      ("+", default_masculine_plural), ("+f", default_feminine_plural)]),
     ("f", "feminine", lambda g: default_feminine),
     ("m", "masculine", lambda g: default_masculine),
     ("sing", "singulative", lambda g: default_feminine),
@@ -209,7 +223,7 @@ def process_text_on_page(index, pagetitle, text):
         inflections = noun_inflections
         genders = getp("2")
         if not genders:
-          if tn == "ar-coll-noun":
+          if tn in ["ar-coll-noun", "ar-noun-nisba", "ar-noun-sound"]:
             genders = "m"
           elif tn == "ar-sing-noun":
             genders = "f"
@@ -228,121 +242,112 @@ def process_text_on_page(index, pagetitle, text):
       split_headsegs = blib.split_alternating_runs(headsegs, ",")
       heads = ["".join(run) for run in split_headsegs]
       parsed_heads = []
-      must_continue = False
       for head in heads:
         headterm, headtr, headother = parse_term_with_tr(head)
         if headterm is None:
           pagemsg("WARNING: Couldn't parse term and translit in head=%s: %s" % (head, str(t)))
-          must_continue = True
-          break
+          raise ContinueException()
         parsed_heads.append((headterm, headtr, headother))
-      if must_continue:
-        continue
       for inflspec in inflections:
-        field, desc, default_fn = inflspec
+        field, desc, default_fns = inflspec
         if genders:
-          default_fn = default_fn(genders)
-        val = getp(field)
-        if val:
-          # split on comma but not inside of <...>
-          segs = blib.parse_balanced_segment_run(val, "<", ">")
-          split_segs = blib.split_alternating_runs(segs, ",")
-          segs = ["".join(run) for run in split_segs]
-          newsegs = []
-          defaults = []
-          for headterm, headtr, headother in parsed_heads:
-            defterm, deftr = default_fn(headterm, headtr)
-            defaults.append((defterm, deftr))
-          if len(defaults) == 1:
-            def_segterm, def_segtr = defaults[0]
-            headterm, headtr, headother = parsed_heads[0]
-            headspec = make_term_tr(headterm, headtr, headother)
-            for seg in segs:
-              segterm, segtr, segother = parse_term_with_tr(seg)
-              if segterm is None:
-                pagemsg("WARNING: Couldn't parse term and translit in %s=%s: %s" % (field, seg, str(t)))
-                must_continue = True
-                break
-              canon_segterm, canon_segtr = canonicalize_form(segterm, segtr)
-              canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
-              if canon_segterm != segterm or canon_segtr != segtr:
-                pagemsg("Canonicalized %s=%s to %s: %s" % (field, seg, canonseg, str(t)))
-              if def_segterm == canon_segterm and def_segtr == canon_segtr:
-                pagemsg("Replaced %s=%s with +: %s" % (field, canonseg, str(t)))
-                newsegs.append("+" + segother)
-                notes.append("replace %s=%s with +%s in {{%s}}" % (field, seg, segother, tn))
-              elif seg == headspec:
-                pagemsg("Replaced %s=%s with ~, standing for the head: %s" % (field, seg, str(t)))
-                newsegs.append("~")
-                notes.append("replace %s=%s with ~ in {{%s}}" % (field, seg, tn))
-              elif canon_segterm != segterm or canon_segtr != segtr:
-                notes.append("canonicalize %s=%s to %s in {{%s}}" % (field, seg, canonseg, tn))
-                newsegs.append(canonseg)
-              else:
-                newsegs.append(seg)
-            if must_continue:
-              break
-          else:
-            canon_segs = []
-            orig_segs = []
-            for seg in segs:
-              segterm, segtr, segother = parse_term_with_tr(seg)
-              if segterm is None:
-                pagemsg("WARNING: Couldn't parse term and translit in %s=%s: %s" % (field, seg, str(t)))
-                must_continue = True
-                break
-              canon_segterm, canon_segtr = canonicalize_form(segterm, segtr)
-              canon_segs.append((canon_segterm, canon_segtr))
-              orig_segs.append((segterm, segtr, segother))
-              canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
-              if canon_segterm != segterm or canon_segtr != segtr:
-                pagemsg("Canonicalized %s=%s to %s: %s" % (field, seg, canonseg, str(t)))
-            if must_continue:
-              break
-            if set(defaults) <= set(canon_segs):
-              saw_plus = False
-              for (canon_segterm, canon_segtr), (segterm, segtr, segother) in zip(canon_segs, orig_segs):
-                seg = make_term_tr(segterm, segtr, segother)
+          default_fns = default_fns(genders)
+        if callable(default_fns):
+          default_fns = [("+", default_fns)]
+        for plus_symbol, default_fn in default_fns:
+          val = getp(field)
+          if val:
+            # split on comma but not inside of <...>
+            segs = blib.parse_balanced_segment_run(val, "<", ">")
+            split_segs = blib.split_alternating_runs(segs, ",")
+            segs = ["".join(run) for run in split_segs]
+            newsegs = []
+            defaults = []
+            for headterm, headtr, headother in parsed_heads:
+              defterm, deftr = default_fn(headterm, headtr)
+              defaults.append((defterm, deftr))
+            if len(defaults) == 1:
+              def_segterm, def_segtr = defaults[0]
+              headterm, headtr, headother = parsed_heads[0]
+              headspec = make_term_tr(headterm, headtr, headother)
+              for seg in segs:
+                segterm, segtr, segother = parse_term_with_tr(seg)
+                if segterm is None:
+                  pagemsg("WARNING: Couldn't parse term and translit in %s=%s: %s" % (field, seg, str(t)))
+                  raise ContinueException()
+                canon_segterm, canon_segtr = canonicalize_form(segterm, segtr)
                 canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
-                if (canon_segterm, canon_segtr) in defaults:
-                  if not saw_plus:
-                    pagemsg("Replaced %s=%s with +: %s" % (field, canonseg, str(t)))
-                    newsegs.append("+" + segother)
-                    notes.append("replace %s=%s with +%s in {{%s}}" % (field, seg, segother, tn))
-                    saw_plus = True
-                elif canon_segterm != segterm or canon_segtr != segtr:
-                  notes.append("canonicalize %s=%s to %s in {{%s}}" % (field, seg, canonseg, tn))
-                  newsegs.append(canonseg)
-                else:
-                  newsegs.append(seg)
-            elif set(parsed_heads) <= set(orig_segs):
-              saw_tilde = False
-              for (canon_segterm, canon_segtr), (segterm, segtr, segother) in zip(canon_segs, orig_segs):
-                seg = make_term_tr(segterm, segtr, segother)
-                canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
-                if (segterm, segtr, segother) in parsed_heads:
-                  if not saw_tilde:
-                    pagemsg("Replaced %s=%s with ~: %s" % (field, seg, str(t)))
-                    newsegs.append("~")
-                    notes.append("replace %s=%s with ~ in {{%s}}" % (field, seg, tn))
-                    saw_tilde = True
+                if canon_segterm != segterm or canon_segtr != segtr:
+                  pagemsg("Canonicalized %s=%s to %s: %s" % (field, seg, canonseg, str(t)))
+                if def_segterm == canon_segterm and def_segtr == canon_segtr:
+                  pagemsg("Replaced %s=%s with %s: %s" % (field, canonseg, plus_symbol, str(t)))
+                  newsegs.append(plus_symbol + segother)
+                  notes.append("replace %s=%s with %s%s in {{%s}}" % (field, seg, plus_symbol, segother, tn))
+                elif seg == headspec:
+                  pagemsg("Replaced %s=%s with ~, standing for the head: %s" % (field, seg, str(t)))
+                  newsegs.append("~")
+                  notes.append("replace %s=%s with ~ in {{%s}}" % (field, seg, tn))
                 elif canon_segterm != segterm or canon_segtr != segtr:
                   notes.append("canonicalize %s=%s to %s in {{%s}}" % (field, seg, canonseg, tn))
                   newsegs.append(canonseg)
                 else:
                   newsegs.append(seg)
             else:
-              for (canon_segterm, canon_segtr), (segterm, segtr, segother) in zip(canon_segs, orig_segs):
-                seg = make_term_tr(segterm, segtr, segother)
+              canon_segs = []
+              orig_segs = []
+              for seg in segs:
+                segterm, segtr, segother = parse_term_with_tr(seg)
+                if segterm is None:
+                  pagemsg("WARNING: Couldn't parse term and translit in %s=%s: %s" % (field, seg, str(t)))
+                  raise ContinueException()
+                canon_segterm, canon_segtr = canonicalize_form(segterm, segtr)
+                canon_segs.append((canon_segterm, canon_segtr))
+                orig_segs.append((segterm, segtr, segother))
                 canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
                 if canon_segterm != segterm or canon_segtr != segtr:
-                  notes.append("canonicalize %s=%s to %s in {{%s}}" % (field, seg, canonseg, tn))
-                  newsegs.append(canonseg)
-                else:
-                  newsegs.append(seg)
-          t.add(field, ",".join(newsegs))
-      if must_continue:
-        continue
+                  pagemsg("Canonicalized %s=%s to %s: %s" % (field, seg, canonseg, str(t)))
+              if set(defaults) <= set(canon_segs):
+                saw_plus = False
+                for (canon_segterm, canon_segtr), (segterm, segtr, segother) in zip(canon_segs, orig_segs):
+                  seg = make_term_tr(segterm, segtr, segother)
+                  canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
+                  if (canon_segterm, canon_segtr) in defaults:
+                    if not saw_plus:
+                      pagemsg("Replaced %s=%s with %s: %s" % (field, canonseg, plus_symbol, str(t)))
+                      newsegs.append(plus_symbol + segother)
+                      notes.append("replace %s=%s with %s%s in {{%s}}" % (field, seg, plus_symbol, segother, tn))
+                      saw_plus = True
+                  elif canon_segterm != segterm or canon_segtr != segtr:
+                    notes.append("canonicalize %s=%s to %s in {{%s}}" % (field, seg, canonseg, tn))
+                    newsegs.append(canonseg)
+                  else:
+                    newsegs.append(seg)
+              elif set(parsed_heads) <= set(orig_segs):
+                saw_tilde = False
+                for (canon_segterm, canon_segtr), (segterm, segtr, segother) in zip(canon_segs, orig_segs):
+                  seg = make_term_tr(segterm, segtr, segother)
+                  canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
+                  if (segterm, segtr, segother) in parsed_heads:
+                    if not saw_tilde:
+                      pagemsg("Replaced %s=%s with ~: %s" % (field, seg, str(t)))
+                      newsegs.append("~")
+                      notes.append("replace %s=%s with ~ in {{%s}}" % (field, seg, tn))
+                      saw_tilde = True
+                  elif canon_segterm != segterm or canon_segtr != segtr:
+                    notes.append("canonicalize %s=%s to %s in {{%s}}" % (field, seg, canonseg, tn))
+                    newsegs.append(canonseg)
+                  else:
+                    newsegs.append(seg)
+              else:
+                for (canon_segterm, canon_segtr), (segterm, segtr, segother) in zip(canon_segs, orig_segs):
+                  seg = make_term_tr(segterm, segtr, segother)
+                  canonseg = make_term_tr(canon_segterm, canon_segtr, segother)
+                  if canon_segterm != segterm or canon_segtr != segtr:
+                    notes.append("canonicalize %s=%s to %s in {{%s}}" % (field, seg, canonseg, tn))
+                    newsegs.append(canonseg)
+                  else:
+                    newsegs.append(seg)
+            t.add(field, ",".join(newsegs))
 
       if tn == "ar-adj":
         f = getp("f")
@@ -367,8 +372,54 @@ def process_text_on_page(index, pagetitle, text):
             else:
               pagemsg("NOTE: Would replace {{ar-adj}} with auto-defaulting {{ar-adj+}} but masculine and feminine plural are missing: %s"
                 % str(t))
+
+      if tn in ["ar-noun-nisba", "ar-noun-sound"]:
+        subnotes = []
+        gender = getp("2")
+        if not gender:
+          param_after_1 = blib.find_following_param(t, "1")
+          t.add("2", "m", before=param_after_1)
+          subnotes.append("add 2=m")
+        if getp("pl") == "+":
+          rmparam(t, "pl")
+          subnotes.append("remove redundant plural")
+        if getp("f") == "+":
+          rmparam(t, "f")
+          subnotes.append("remove redundant feminine")
+        blib.set_template_name(t, "ar-noun-anim")
+        subnote_text = "; " + ", ".join(subnotes) if subnotes else ""
+        pagemsg("Convert {{%s}} to {{ar-noun-anim}}%s" % (tn, subnote_text))
+        notes.append("convert {{%s}} to {{ar-noun-anim}}%s" % (tn, subnote_text))
+
+      if tn == "ar-noun" and getp("pl") and (genders == "m" and getp("f") == "+" or genders == "f" and getp("m") == "+"):
+        subnotes = []
+        if getp("pl") == "+":
+          rmparam(t, "pl")
+          subnotes.append("remove redundant plural")
+        if genders == "m" and getp("f") == "+":
+          rmparam(t, "f")
+          subnotes.append("remove redundant feminine")
+        if genders == "f" and getp("m") == "+":
+          rmparam(t, "m")
+          subnotes.append("remove redundant masculine")
+        blib.set_template_name(t, "ar-noun-anim")
+        subnote_text = "; " + ", ".join(subnotes) if subnotes else ""
+        pagemsg("Convert {{ar-noun}} to {{ar-noun-anim}}%s" % subnote_text)
+        notes.append("convert {{ar-noun}} to {{ar-noun-anim}}%s" % subnote_text)
+
+      if getp("d") == "+":
+        pagemsg("Removing redundant d=+: %s" % str(t))
+        rmparam(t, "d")
+        notes.append("remove redundant d=+")
+      if getp("fd") == "+":
+        pagemsg("Removing redundant fd=+: %s" % str(t))
+        rmparam(t, "fd")
+        notes.append("remove redundant fd=+")
+
     except BadTranslitException as e:
       pagemsg("%s: %s" % (str(e), str(t)))
+      continue
+    except ContinueException as ce:
       continue
 
   return ar.undo_reorder_shadda(str(parsed)), notes
