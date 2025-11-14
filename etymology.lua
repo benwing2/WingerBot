d951709218e5ecbfb721dd45e5d8e0753b108d39
Ben Wing <ben@benwing.com>
2025-11-14 02:26:14 -0600

    etymology: latest from Wiktionary [WARNING: OVERWRITES PARTIAL WORK ON FORMAT-UTILITIES AND 'use joinSegments in [[Module:table]]'

--- etymology.lua
+++ etymology.lua
@@ -3,8 +3,6 @@ local export = {}
 -- For testing
 local force_cat = false
 
-local require_when_needed = require("Module:utilities/require when needed")
-
 local debug_track_module = "Module:debug/track"
 local languages_module = "Module:languages"
 local links_module = "Module:links"
@@ -12,43 +10,88 @@ local pron_qualifier_module = "Module:pron qualifier"
 local table_module = "Module:table"
 local utilities_module = "Module:utilities"
 
-local m_links = require_when_needed(links_module)
-local m_table = require_when_needed(table_module)
-local m_utilities = require_when_needed(utilities_module)
+local concat = table.concat
+local insert = table.insert
+local new_title = mw.title.new
 
-local function join_segments(segs, conj)
+local function debug_track(...)
+	debug_track = require(debug_track_module)
+	return debug_track(...)
+end
+
+local function format_categories(...)
+	format_categories = require(utilities_module).format_categories
+	return format_categories(...)
+end
+
+local function format_qualifiers(...)
+	format_qualifiers = require(pron_qualifier_module).format_qualifiers
+	return format_qualifiers(...)
+end
+
+local function full_link(...)
+	full_link = require(links_module).full_link
+	return full_link(...)
+end
+
+local function get_language_data_module_name(...)
+	get_language_data_module_name = require(languages_module).getDataModuleName
+	return get_language_data_module_name(...)
+end
+
+local function get_link_page(...)
+	get_link_page = require(links_module).get_link_page
+	return get_link_page(...)
+end
+
+local function language_link(...)
+	language_link = require(links_module).language_link
+	return language_link(...)
+end
+
+local function serial_comma_join(...)
+	serial_comma_join = require(table_module).serialCommaJoin
+	return serial_comma_join(...)
+end
+
+local function shallow_copy(...)
+	shallow_copy = require(table_module).shallowCopy
+	return shallow_copy(...)
+end
+
+local function track(page, code)
+	local tracking_page = "etymology/" .. page
+	debug_track(tracking_page)
+	if code then
+		debug_track(tracking_page .. "/" .. code)
+	end
+end
+
+local function join_segs(segs, conj)
 	if not segs[2] then
 		return segs[1]
-	else
-		return m_table.joinSegments(segs, {conj = conj})
+	elseif conj == "and" or conj == "or" then
+		return serial_comma_join(segs, {conj = conj})
 	end
+	local sep
+	if conj == "," or conj == ";" then
+		sep = conj .. " "
+	elseif conj == "/" then
+		sep = "/"
+	elseif conj == "~" then
+		sep = " ~ "
+	elseif conj then
+		error(("Internal error: Unrecognized conjunction \"%s\""):format(conj))
+	else
+		error(("Internal error: No value supplied for conjunction"):format(conj))
+	end
+	return concat(segs, sep)
 end
 
-local function create_one_link(termobj, template_name)
-	if termobj.lang:hasType("family") then
-		if termobj.term and termobj.term ~= "-" then
-			require(debug_track_module)(template_name .. "/family-with-term")
-		end
-		
-		termobj.term = "-"
-	end
-	template_name = template_name or "derived"
-	local link = ""
-	if termobj.term == "-" then
-		--[=[
-		[[Special:WhatLinksHere/Wiktionary:Tracking/cognate/no-term]]
-		[[Special:WhatLinksHere/Wiktionary:Tracking/derived/no-term]]
-		[[Special:WhatLinksHere/Wiktionary:Tracking/borrowed/no-term]]
-		[[Special:WhatLinksHere/Wiktionary:Tracking/calque/no-term]]
-		]=]
-		require(debug_track_module)(template_name .. "/no-term")
-	else
-		link = m_links.full_link(termobj, "term", nil, "show qualifiers")
-	end
-	
-	return link
+-- Returns true if `lang` is the same as `source`, or a variety of it.
+local function lang_is_source(lang, source)
+	return lang:getCode() == source:getCode() or lang:hasParent(source)
 end
-
 
 -- Format one or more links as specified in `termobjs`, a list of term objects of the format accepted by full_link() in
 -- [[Module:links]], additionally with optional qualifiers, labels and references. `conj` is used to join multiple
@@ -57,16 +100,53 @@ end
 -- the case unless there is a single term with the value "-").
 function export.format_links(termobjs, conj, template_name)
 	for i, termobj in ipairs(termobjs) do
-		termobjs[i] = create_one_link(termobj, template_name)
+		local term = termobj.term
+		if termobj.lang:hasType("family") then
+			if term and term ~= "-" then
+				debug_track(template_name .. "/family-with-term")
+			end
+			term = "-"
+			termobj.term = term
+		end
+		template_name = template_name or "derived"
+		if term == "-" then
+			--[=[
+			[[Special:WhatLinksHere/Wiktionary:Tracking/cognate/no-term]]
+			[[Special:WhatLinksHere/Wiktionary:Tracking/derived/no-term]]
+			[[Special:WhatLinksHere/Wiktionary:Tracking/borrowed/no-term]]
+			[[Special:WhatLinksHere/Wiktionary:Tracking/calque/no-term]]
+			]=]
+			debug_track(template_name .. "/no-term")
+			termobjs[i] = ""
+		else
+			termobjs[i] = full_link(termobj, "term", nil, "show qualifiers")
+		end
 	end
 
-	local retval = join_segments(termobjs, conj)
+	local retval = join_segs(termobjs, conj)
 	if retval ~= "" then
 		retval = " " .. retval
 	end
 	return retval
 end
-	
+
+function export.get_display_and_cat_name(source, raw)
+	local display, cat_name
+	if source:getCode() == "und" then
+		display = "undetermined"
+		cat_name = "other languages"
+	elseif source:getCode() == "mul" then
+		display = raw and "translingual" or "[[w:Translingualism|translingual]]"
+		cat_name = "Translingual"
+	elseif source:getCode() == "mul-tax" then
+		display = raw and "taxonomic name" or "[[w:Biological nomenclature|taxonomic name]]"
+		cat_name = "taxonomic names"
+	else
+		display = raw and source:getCanonicalName() or source:makeWikipediaLink()
+		cat_name = source:getDisplayForm()
+	end
+	return display, cat_name
+end
 
 function export.insert_source_cat_get_display(data)
 	local categories, lang, source = data.categories, data.lang, data.source
@@ -82,37 +162,32 @@ function export.insert_source_cat_get_display(data)
 		-- If `lang` is an etym-only language, we need to check both it and its parent full language against `source`.
 		-- Otherwise if e.g. `lang` is Medieval Latin and `source` is Latin, we'll end up wrongly constructing a
 		-- category 'Latin terms derived from Latin'.
-		if lang:getCode() == source:getCode() or lang:getFullCode() == source:getCode() then
-			table.insert(categories, langname .. " terms borrowed back into " .. langname)
-		else
-			table.insert(categories, langname .. " " .. (data.borrowing_type or "terms derived") .. " from " ..
-				cat_name)
-		end
+		insert(categories, langname .. (
+			lang_is_source(lang, source) and " terms borrowed back into " .. cat_name or
+			" " .. (data.borrowing_type or "terms derived") .. " from " .. cat_name
+		))
 	end
 
 	return display, categories
 end
 
-
 function export.format_source(data)
 	local lang, sort_key = data.lang, data.sort_key
 	-- [[Special:WhatLinksHere/Wiktionary:Tracking/etymology/sortkey]]
 	if sort_key then
-		require(debug_track_module)("etymology/sortkey")
+		track("sortkey")
 	end
 
 	local display, categories = export.insert_source_cat_get_display(data)
 	if lang and not data.nocat then
 		-- Format categories, but only if there is a current language; {{cog}} currently gets no categories
-		categories = m_utilities.format_categories(categories, lang, sort_key, nil,
-			data.force_cat or force_cat)
+		categories = format_categories(categories, lang, sort_key, nil, data.force_cat or force_cat)
 	else
 		categories = ""
 	end
 	
 	return "<span class=\"etyl\">" .. display .. categories .. "</span>"
 end
-
 
 --[==[
 Format sources for etymology templates such as {{tl|bor}}, {{tl|der}}, {{tl|inh}} and {{tl|cog}}. There may potentially
@@ -143,59 +218,60 @@ source language will be linked to the Wikipedia entry for the language, just as 
 * `force_cat`: Force category generation on non-mainspace pages.
 ]==]
 function export.format_sources(data)
-	local lang, sources, terms, sort_key, categories, nocat, sourceconj =
-		data.lang, data.sources, data.terms, data.sort_key, data.categories, data.nocat, data.sourceconj
-	
-	local source_segs = {}
-	final_link_page = sources[2] and terms[1].term and terms[1].term ~= "-" and
-		m_links.get_link_page(terms[1].term, sources[#sources], terms[1].sc) or nil
+	local lang, sources, terms, borrowing_type, sort_key, categories, nocat =
+		data.lang, data.sources, data.terms, data.borrowing_type, data.sort_key, data.categories, data.nocat
+	local term1, sources_n, source_segs, final_link_page = terms[1], #sources, {}
+	local term1_term, term1_sc = term1.term, term1.sc
+	if sources_n > 1 and term1_term and term1_term ~= "-" then
+		final_link_page = get_link_page(term1_term, sources[sources_n], term1_sc)
+	end
 	for i, source in ipairs(sources) do
-		local seg
-		local display_term
-		if i < #sources and terms[1].term and terms[1].term ~= "-" then
-			local link_page = m_links.get_link_page(terms[1].term, source, terms[1].sc)
-			local exists = link_page and mw.title.new(link_page).exists
-			local different = link_page ~= final_link_page
-			display_term = exists or different
+		local seg, display_term
+		if i < sources_n and term1_term and term1_term ~= "-" then
+			local link_page = get_link_page(term1_term, source, term1_sc)
+			display_term = (link_page ~= final_link_page) or (link_page and new_title(link_page).exists)
 		end
+		-- TODO: if the display forms or transliterations are different, display the terms separately.
 		if display_term then
-			local display, this_cats = export.insert_source_cat_get_display {
-				categories = categories,
+			local display, this_cats = export.insert_source_cat_get_display{
 				lang = lang,
 				source = source,
+				borrowing_type = borrowing_type,
 				raw = true,
+				categories = categories,
 				nocat = nocat,
-				borrowing_type = data.borrowing_type,
 			}
-			seg = m_links.language_link {
-				lang = source, term = terms[1].term, alt = display, tr = "-"
+			seg = language_link{
+				lang = source,
+				term = term1_term,
+				alt = display,
+				tr = "-",
 			}
 			if lang and not nocat then
 				-- Format categories, but only if there is a current language; {{cog}} currently gets no categories
-				this_cats = m_utilities.format_categories(this_cats, lang, sort_key, nil, data.force_cat or force_cat)
+				this_cats = format_categories(this_cats, lang, sort_key, nil, data.force_cat or force_cat)
 			else
 				this_cats = ""
 			end
 			seg = "<span class=\"etyl\">" .. seg .. this_cats .. "</span>"
 		else
-			seg = export.format_source {
+			seg = export.format_source{
 				lang = lang,
 				source = source,
+				borrowing_type = borrowing_type,
 				sort_key = sort_key,
 				categories = categories,
 				nocat = nocat,
-				borrowing_type = borrowing_type,
 			}
 		end
-		table.insert(source_segs, seg)
+		insert(source_segs, seg)
 	end
-	return join_segments(source_segs, sourceconj or "and")
+	return join_segs(source_segs, data.sourceconj or "and")
 end
-
 
 -- Internal implementation of {{cognate}}/{{cog}} template.
 function export.format_cognate(data)
-	return export.format_derived {
+	return export.format_derived{
 		sources = data.sources,
 		terms = data.terms,
 		sort_key = data.sort_key,
@@ -206,16 +282,16 @@ function export.format_cognate(data)
 	}
 end
 
-
 -- Internal implementation of {{derived}}/{{der}} template. This is called externally from [[Module:affix]],
 -- [[Module:affixusex]] and [[Module:see]] and needs to support qualifiers, labels and references on the outside
 -- of the sources for use by those modules.
 function export.format_derived(data)
-	local result = export.format_sources(data) .. export.format_links(data.terms, data.conj, data.template_name)
+	local terms = data.terms
+	local result = export.format_sources(data) .. export.format_links(terms, data.conj, data.template_name)
 	local q, qq, l, ll, refs = data.q, data.qq, data.l, data.ll, data.refs
 	if q and q[1] or qq and qq[1] or l and l[1] or ll and ll[1] or refs and refs[1] then
-		result = require(pron_qualifier_module).format_qualifiers {
-			lang = data.terms[1].lang,
+		result = format_qualifiers{
+			lang = terms[1].lang,
 			text = result,
 			q = q,
 			qq = qq,
@@ -227,37 +303,32 @@ function export.format_derived(data)
 	return result
 end
 
-
 function export.insert_borrowed_cat(categories, lang, source)
-	local category
-	-- Do the same check as in insert_source_cat_get_display() (inverted).
-	if not (lang:getCode() == source:getCode() or lang:getFullCode() == source:getCode()) then
-		-- If both are the same, we want e.g. [[:Category:English terms borrowed back into English]] not
-		-- [[:Category:English terms borrowed from English]]; the former is inserted automatically by format_source().
-		-- The second parameter here doesn't matter as it only affects `display`, which we don't use.
-		local display, cat_name = export.get_display_and_cat_name(source, "raw")
-		category = " terms borrowed from " .. cat_name
+	if lang_is_source(lang, source) then
+		return
 	end
-	if category then
-		table.insert(categories, lang:getFullName() .. category)
-	end
+	-- If both are the same, we want e.g. [[:Category:English terms borrowed back into English]] not
+	-- [[:Category:English terms borrowed from English]]; the former is inserted automatically by format_source().
+	-- The second parameter here doesn't matter as it only affects `display`, which we don't use.
+	insert(categories, lang:getFullName() .. " terms borrowed from " .. select(2, export.get_display_and_cat_name(source, "raw")))
 end
-
 
 -- Internal implementation of {{borrowed}}/{{bor}} template.
 function export.format_borrowed(data)
-	data = m_table.shallowCopy(data)
-	data.categories = {}
+	local categories = {}
 
 	if not data.nocat then
+		local lang = data.lang
 		for _, source in ipairs(data.sources) do
-			export.insert_borrowed_cat(data.categories, data.lang, source)
+			export.insert_borrowed_cat(categories, lang, source)
 		end
 	end
 
+	data = shallow_copy(data)
+	data.categories = categories
+
 	return export.format_sources(data) .. export.format_links(data.terms, data.conj, "borrowed")
 end
-
 
 do
 	-- Generate the non-ancestor error message.
@@ -275,30 +346,35 @@ do
 	function export.check_ancestor(lang, otherlang)
 		-- FIXME: I don't know if this function works correctly with etym-only languages in `lang`. I have fixed up
 		-- the module link code appropriately (June 2024) but the remaining logic is untouched.
-		if lang:hasAncestor(otherlang) or mw.title.getCurrentTitle().nsText == "Template" then
+		if lang:hasAncestor(otherlang) then
+			-- [[Special:WhatLinksHere/Wiktionary:Tracking/etymology/variety]]
+			-- Track inheritance from varieties of Latin that shouldn't have any descendants (everything except Old Latin, Classical Latin and Vulgar Latin).
+			if otherlang:getFullCode() == "la" then
+				otherlang = otherlang:getCode()
+				if not (otherlang == "itc-ola" or otherlang == "la-cla" or otherlang == "la-vul") then
+					track("bad ancestor", otherlang)
+				end
+			end
 			return
 		end
 		local ancestors, postscript = lang:getAncestors()
 		local etym_module_link = lang:hasType("etymology-only") and "[[Module:etymology languages/data]] or " or ""
-		local module_link = "[[Module:"
-			.. require(languages_module).getDataModuleName(lang:getFullCode())
-			.. "]]"
+		local module_link = "[[" .. get_language_data_module_name(lang:getFullCode()) .. "]]"
 		if not ancestors[1] then
 			postscript = show_language(lang) .. " has no ancestors."
 		else
 			local ancestor_list = {}
 			for _, ancestor in ipairs(ancestors) do
-				table.insert(ancestor_list, show_language(ancestor))
+				insert(ancestor_list, show_language(ancestor))
 			end
 			postscript = ("The ancestor%s of %s %s %s."):format(
 				ancestors[2] and "s" or "", lang:getCanonicalName(),
-				ancestors[2] and "are" or "is", table.concat(ancestor_list, " and "))
+				ancestors[2] and "are" or "is", concat(ancestor_list, " and "))
 		end
 		error(("%s is not set as an ancestor of %s in %s%s. %s")
 			:format(show_language(otherlang), show_language(lang), etym_module_link, module_link, postscript))
 	end
 end
-
 
 -- Internal implementation of {{inherited}}/{{inh}} template.
 function export.format_inherited(data)
@@ -307,12 +383,12 @@ function export.format_inherited(data)
 	
 	local categories = {}
 	if not nocat then
-		table.insert(categories, lang:getFullName() .. " terms inherited from " .. source:getCanonicalName())
+		insert(categories, lang:getFullName() .. " terms inherited from " .. source:getCanonicalName())
 	end
 
 	export.check_ancestor(lang, source)
 
-	return export.format_source {
+	return export.format_source{
 		lang = lang,
 		source = source,
 		sort_key = sort_key,
@@ -323,58 +399,46 @@ function export.format_inherited(data)
 	
 end
 
-
 -- Internal implementation of "misc variant" templates such as {{abbrev}}, {{clipping}}, {{reduplication}} and the like.
 function export.format_misc_variant(data)
-	local lang, notext, text, oftext, terms, conj, nocat, cats =
-		data.lang, data.notext, data.text, data.oftext, data.terms, data.conj, data.nocat, data.cats
-
-	local parts = {}
-
-	local function ins(txt)
-		table.insert(parts, txt)
-	end
+	local lang, notext, terms, cats, parts = data.lang, data.notext, data.terms, data.cats, {}
 
 	if not notext then
-		ins(text)
+		insert(parts, data.text)
 	end
 	if terms[1] then
 		if not notext then
-			ins(" ")
-			ins(oftext or "of")
-			ins(" ")
+			insert(parts, " " .. (data.oftext or "of") .. " ")
 		end
-		ins(export.format_links(terms, conj, "misc_variant"))
+		insert(parts, export.format_links(terms, data.conj, "misc_variant"))
 	end
 
 	local categories = {}
-	if not nocat and cats then
+	if not data.nocat and cats then
 		for _, cat in ipairs(cats) do
-			table.insert(categories, lang:getFullName() .. " " .. cat)
+			insert(categories, lang:getFullName() .. " " .. cat)
 		end
 	end
 	if #categories > 0 then
-		ins(m_utilities.format_categories(categories, lang, data.sort_key, nil, data.force_cat or force_cat))
+		insert(parts, format_categories(categories, lang, data.sort_key, nil, data.force_cat or force_cat))
 	end
 
-	return table.concat(parts)
+	return concat(parts)
 end
 
 -- Implementation of miscellaneous templates such as {{unknown}} and {{onomatopoeia}} that have no associated terms.
 function export.format_misc_variant_no_term(data)
-	local lang = data.lang
-
 	local parts = {}
 	if not data.notext then
-		table.insert(parts, data.title)
+		insert(parts, data.title)
 	end
 	if not data.nocat and data.cat then
-		local categories = {}
-		table.insert(categories, lang:getFullName() .. " " .. data.cat)
-		table.insert(parts, m_utilities.format_categories(categories, lang, data.sort_key, nil, data.force_cat or force_cat))
+		local lang, categories = data.lang, {}
+		insert(categories, lang:getFullName() .. " " .. data.cat)
+		insert(parts, format_categories(categories, lang, data.sort_key, nil, data.force_cat or force_cat))
 	end
 
-	return table.concat(parts)
+	return concat(parts)
 end
 
 return export
