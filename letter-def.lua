8ab0c2f8b0fa3c4eb8fe7862fb2b6d5e62127ea6
Ben Wing <ben@benwing.com>
2026-01-12 18:37:50 -0600

    letter-def: latest from Wiktionary (allow multiple comma-separated items and inline modifiers in prec=/foll=, names, etc.; support *graphs as letters; fix bug in indef handling; trim whitespace in ine())

--- letter-def.lua
+++ letter-def.lua
@@ -14,6 +14,7 @@ local links_module = "Module:links"
 local parameters_module = "Module:parameters"
 local scripts_module = "Module:scripts"
 local string_utilities_module = "Module:string utilities"
+local table_module = "Module:table"
 local utilities_module = "Module:utilities"
 
 local m_links = require(links_module)
@@ -22,6 +23,7 @@ local m_string_utilities = require(string_utilities_module)
 local glossary_link = require_when_needed(headword_utilities_module, "glossary_link")
 local lang_getByCode = require_when_needed(languages_module, "getByCode")
 local format_categories = require_when_needed(utilities_module, "format_categories")
+local serial_comma_join = require_when_needed(table_module, "serialCommaJoin")
 
 local uupper = m_string_utilities.upper
 local ulower = m_string_utilities.lower
@@ -30,6 +32,10 @@ local insert = table.insert
 local concat = table.concat
 
 local function ine(val)
+	if not val then
+		return val
+	end
+	val = mw.text.trim(val)
 	if val == "" then return nil else return val end
 end
 
@@ -47,6 +53,54 @@ local function ordinal_to_word(num)
 		true -- ordinal
 	)
 end
+
+local function add_initial_colon_to_term(term)
+	if term ~= "-" and term ~= "+" and not term:find("^:") then
+		-- Make sure we link to the specified term even if it has a diacritic that would normally be stripped off.
+		term = ":" .. term
+	end
+	return term
+end
+
+local function parse_param(value, paramname)
+	if not value then
+		return nil
+	end
+	local termobjs
+	if value:find("[,<]") then
+		termobjs = require(headword_utilities_module).parse_term_with_modifiers {
+			val = value,
+			paramname = paramname,
+			splitchar = ",",
+			include_mods = {"tr", "ts", "t", "sc"},
+		}
+	else
+		termobjs = {{ term = value }}
+	end
+	return termobjs
+end
+
+local function parse_char(value, paramname, no_prefix_colon)
+	if not value then
+		return nil
+	end
+	local termobjs = parse_param(value, paramname)
+	for _, termobj in ipairs(termobjs) do
+		if not no_prefix_colon then
+			termobj.term = add_initial_colon_to_term(termobj.term)
+		end
+	end
+	return termobjs
+end
+
+local function join_items(items, conj)
+	if not items[2] then
+		return items[1]
+	else
+		return serial_comma_join(items, {conj = conj})
+	end
+end
+
 
 --[==[
 Implementation of {{tl|Latn-def}}, {{tl|Cyrl-def}} and the like. Supports the following invocation parameter:
@@ -66,15 +120,18 @@ function export.show(frame)
 	})
 	local parent_args = frame:getParent().args
 	local deftype = ine(parent_args[2])
-	local orig_deftype = deftype
 	if deftype == "ordinal" then
 		deftype = "numsym"
 	end
+	local canontype = deftype
+	if canontype == "digraph" or canontype == "trigraph" or canontype == "tetragraph" or canontype == "multigraph" then
+		canontype = "letter"
+	end
 	-- FIXME: convert 'ordinal' to 'numsym'
-	local deftypes = {"letter", "ordinal", "numsym", "name", "diacritic", "syllable"}
+	local deftypes = {"letter", "digraph", "trigraph", "tetragraph", "multigraph", "numsym", "ordinal", "name", "diacritic", "syllable"}
 	local params = {
 		[1] = {type = "language", required = true, template_default = "und"},
-		[2] = {set = deftypes},
+		[2] = {set = deftypes, required = true},
 		sc = {type = "script"},
 		nocap = boolean_param,
 		dot = true,
@@ -87,7 +144,7 @@ function export.show(frame)
 			params[k] = v
 		end
 	end
-	if deftype == "letter" or deftype == "numsym" then
+	if canontype == "letter" or canontype == "numsym" then
 		merge_params {
 			[3] = {type = "number"},
 			[4] = list_param,
@@ -99,7 +156,7 @@ function export.show(frame)
 			foll = true,
 			last = boolean_param,
 		}
-	elseif deftype == "name" then
+	elseif canontype == "name" then
 		merge_params {
 			[3] = {required = true},
 			[4] = true,
@@ -109,7 +166,7 @@ function export.show(frame)
 			lit = true,
 			eq = true,
 		}
-	elseif deftype == "diacritic" then
+	elseif canontype == "diacritic" then
 		merge_params {
 			[3] = list_param,
 			name = list_param,
@@ -119,32 +176,23 @@ function export.show(frame)
 			moreexamples = boolean_param,
 			t = {list = true, allow_holes = true},
 		}
-	elseif deftype == "syllable" then
+	elseif canontype == "syllable" then
 		merge_params {
 			[3] = {required = true},
 			[4] = {required = true},
 			[5] = {required = true},
 		}
 	else
-		local possible_deftypes = {}
-		for _, possible in ipairs(deftypes) do
-			insert(possible_deftypes, ("'%s'"):format(possible))
-		end
-		possible_deftypes = mw.text.listToText(possible_deftypes, nil, " or ")
-		if deftype then
-			error(("Invalid value '%s' for 2=, should be one of %s"):format(orig_deftype, possible_deftypes))
-		else
-			error(("Missing value for 2=, should be one of %s"):format(possible_deftypes))
-		end
+		-- no need to throw an error here, as it will be thrown by [[Module:parameters]]
 	end
 	local args = require(parameters_module).process(parent_args, params)
 	local lang = args[1]
 	local sc = args.sc or iargs.sc
 
 	if not sc then
-		if deftype == "letter" or deftype == "numsym" then
+		if canontype == "letter" or canontype == "numsym" then
 			sc = lang:findBestScript(args.pagename or mw.loadData("Module:headword/data").pagename)
-		elseif deftype == "diacritic" or deftype == "name" then
+		elseif canontype == "diacritic" or canontype == "name" then
 			local test_char = args[3]
 			if type(test_char) == "table" then
 				test_char = test_char[1]
@@ -164,34 +212,44 @@ function export.show(frame)
 	local linked_script = ("[[Appendix:%s|%s]]"):format(sccatname, sccatname)
 	local categories = {}
 	ins("<span class='use-with-mention'>")
-	local function link_to_lang_or_mul(char)
-		-- Either link a character using the language in 1= or using 'mul' (Translingual). We do this to avoid
-		-- yellow links from trying to link to a nonexistent character. Basically, if linklang=1, we always link
-		-- using the language in 1=; otherwise we try to see if the character is in the language's standardChars,
-		-- and if not, link to Translingual. If the standardChars for the language is missing or the character can't
-		-- be looked up (e.g. it's a digraph or trigraph), assume it's in the language and link using the language.
-		local lang_for_linking
-		if args.linklang then
-			lang_for_linking = lang
-		elseif #char > 1 then
-			-- If the character is a digraph or trigraph, we can't check it against standardChars, which only lists
-			-- single Unicode chars.
-			lang_for_linking = lang
-		else
-			local standard_chars = lang:getStandardCharacters(sc)
-			if type(standard_chars) ~= "string" or ufind(standard_chars, char) then
-				-- No standardChars, or character in standardChars; link using lang.
+	local function link_to_lang_or_mul(char, paramname, notr)
+		local termobjs = parse_char(char, paramname)
+		for i, termobj in ipairs(termobjs) do
+			-- Either link a character using the language in 1= or using 'mul' (Translingual). We do this to avoid
+			-- yellow links from trying to link to a nonexistent character. Basically, if linklang=1, we always link
+			-- using the language in 1=; otherwise we try to see if the character is in the language's standardChars,
+			-- and if not, link to Translingual. If the standardChars for the language is missing or the character can't
+			-- be looked up (e.g. it's a digraph or trigraph), assume it's in the language and link using the language.
+			local lang_for_linking
+			local char = termobj.term
+			if args.linklang then
+				lang_for_linking = lang
+			elseif #char > 1 then
+				-- If the character is a digraph or trigraph, we can't check it against standardChars, which only lists
+				-- single Unicode chars.
 				lang_for_linking = lang
 			else
-				lang_for_linking = lang_getByCode("mul", true)
+				local standard_chars = lang:getStandardCharacters(sc)
+				if type(standard_chars) ~= "string" or ufind(standard_chars, char) then
+					-- No standardChars, or character in standardChars; link using lang.
+					lang_for_linking = lang
+				else
+					lang_for_linking = lang_getByCode("mul", true)
+				end
 			end
+			termobj.lang = lang_for_linking
+			termobj.sc = sc
+			if notr and not termobj.tr then
+				termobj.tr = "-"
+			end
+			termobjs[i] = full_link(termobj, "term")
 		end
-		return full_link({ lang = lang_for_linking, term = char, tr = "-", sc = sc}, "term")
+		return join_items(termobjs, "or")
 	end
 
-	if deftype == "letter" or deftype == "numsym" then
+	if canontype == "letter" or canontype == "numsym" then
 		local indef = not args[3] and not args.last
-		local article = args.indef and "A" or "The"
+		local article = indef and "A" or "The"
 		if args.nocap then
 			article = article:lower()
 		end
@@ -212,10 +270,12 @@ function export.show(frame)
 		local is_mul = lang:getFullCode() == "mul"
 		local lang_for_linking = is_mul and lang_getByCode("en", true) or lang
 		ins(" ")
-		if deftype == "numsym" then
+		if canontype == "numsym" then
 			ins("[[numeral]] [[symbol]]")
+		elseif args[3] and deftype ~= "letter" then
+			ins("[[letter]] (a [[" .. deftype .. "]])")
 		else
-			ins("[[letter]]")
+			ins("[[" .. deftype .. "]]")
 		end
 		ins(" of ")
 		if args.alphabet then
@@ -240,8 +300,15 @@ function export.show(frame)
 		if args[4][1] then
 			ins(", called ")
 			local formatted_names = {}
-			for _, name in ipairs(args[4]) do
-				insert(formatted_names, full_link({lang = lang_for_linking, term = name, gloss = args.t[i]}, "term"))
+			for i, name in ipairs(args[4]) do
+				local nameobjs = parse_param(name, i + 3)
+				for _, nameobj in ipairs(nameobjs) do
+					nameobj.lang = lang_for_linking
+					if args.t[i] then
+						nameobj.gloss = args.t[i]
+					end
+					insert(formatted_names, full_link(nameobj, "term"))
+				end
 			end
 			ins(mw.text.listToText(formatted_names, nil, " or "))
 			if not is_mul then
@@ -255,7 +322,7 @@ function export.show(frame)
 		end
 		if args.prec then
 			ins("; preceded by ")
-			ins(link_to_lang_or_mul(args.prec))
+			ins(link_to_lang_or_mul(args.prec, "prec"))
 		end
 		if args.foll then
 			if args.prec then
@@ -264,20 +331,20 @@ function export.show(frame)
 				ins("; ")
 			end
 			ins("followed by ")
-			ins(link_to_lang_or_mul(args.foll))
+			ins(link_to_lang_or_mul(args.foll, "foll"))
 		end
-		if deftype == "numsym" then
+		if canontype == "numsym" then
 			-- FIXME: Rethink the name of this category.
 			insert(categories, ("%s ordinal numbers"):format(lang:getFullName()))
 		end
 
-	elseif deftype == "name" then
+	elseif canontype == "name" then
 		ins(args.nocap and "the" or "The")
 		ins((" name of the %s letter "):format(linked_script))
-		ins(link_to_lang_or_mul(args[3]))
+		ins(link_to_lang_or_mul(args[3], 3, "notr"))
 		if args[4] then
 			ins("/")
-			ins(link_to_lang_or_mul(args[4]))
+			ins(link_to_lang_or_mul(args[4], 4, "notr"))
 		end
 		if args.alphabet then
 			ins(", in " .. args.alphabet)
@@ -299,7 +366,7 @@ function export.show(frame)
 		end
 		insert(categories, ("%s:%s letter names"):format(lang:getFullCode(), scname))
 
-	elseif deftype == "diacritic" then
+	elseif canontype == "diacritic" then
 		ins(args.nocap and "a" or "A")
 		ins((" [[diacritical mark]] of the %s"):format(linked_script))
 		if args.alphabet then
@@ -314,7 +381,14 @@ function export.show(frame)
 			ins(", called ")
 			local formatted_names = {}
 			for i, name in ipairs(args.name) do
-				insert(formatted_names, full_link({lang = lang, term = name, gloss = args.t[i]}, "term"))
+				local nameobjs = parse_param(name, "name" .. (i == 1 and "" or i))
+				for _, nameobj in ipairs(nameobjs) do
+					nameobj.lang = lang
+					if args.t[i] then
+						nameobj.gloss = args.t[i]
+					end
+					insert(formatted_names, full_link(nameobj, "term"))
+				end
 			end
 			ins(mw.text.listToText(formatted_names, nil, " or "))
 		end
@@ -343,7 +417,7 @@ function export.show(frame)
 			end
 		end
 
-	elseif deftype == "syllable" then
+	elseif canontype == "syllable" then
 		ins(args.nocap and "the " or "The ")
 		if args[3] ~= "-" then
 			ins("[[Appendix:Hiragana script|hiragana]] syllable ")
@@ -355,7 +429,7 @@ function export.show(frame)
 		ins(" in [[Hepburn]] romanization")
 
 	else
-		error(("Internal error: Unhandled deftype %s"):format(mw.dumpObject(deftype)))
+		error(("Internal error: Unhandled canontype %s"):format(mw.dumpObject(canontype)))
 	end
 	local addl = args.addl
 	if addl then
